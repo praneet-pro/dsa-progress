999b54939466aac6332d1fa3aa4b46e472b3a925
Praneet <spraneet333@gmail.com>
2026-03-02 15:24:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,14 +1,31 @@
+/*
+Topic: Greedy / State Compression
+
+Invariant:
+- first  = smallest value seen so far
+- second = smallest possible value > first
+
+Idea:
+- Try to build an increasing sequence of length 3
+- Update first and second greedily to keep them minimal
+- If we ever find a number > second → triplet exists
+
+Why this works:
+- Smaller first/second gives more room for a third number
+- We don’t need the actual sequence, only existence
+
+Time: O(n)
+Space: O(1)
+*/
+
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        vector<int> tails;
+        int first = INT_MAX, second = INT_MAX;
         for(int x : nums) {
-            auto it = lower_bound(tails.begin(), tails.end(), x);
-            if(it == tails.end())
-                tails.push_back(x);
-            else
-                *it = x;
-            if(tails.size() > 2) return true;
+            if(x <= first) first = x;
+            else if(x <= second) second = x;
+            else return true;
         }
         return false;
     }

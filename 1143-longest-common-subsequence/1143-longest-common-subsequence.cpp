550f3a42b7df236f64c613bbaeccda87f8abc490
Praneet <spraneet333@gmail.com>
2026-03-04 18:41:48 +0530

    Time: 14 ms (94.35%), Space: 9.1 MB (97.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,41 @@
+/*
+Dynamic Programming
+
+Approach : 
+- Used 2D array to store the count with the 1st row and coloumn being empty.
+- Empty row and coloumn represented as 0 gives a correct base conditin
+  for mathematical base condition.
+
+Optimization: 
+- Optimized the storage from O(n * m) to O(n).
+- Prev stores the diagonal element value.
+- Temp stores the prev rows diagonal value before it is overwritten.
+
+Time Complexity: O(n * m)
+Space Complexity: O(n)  (optimized)
+*/
+
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        if(m < n) 
+            return longestCommonSubsequence(text2, text1);
+        vector<int> dp(n + 1, 0);
 
         for(int i = 1; i <= m; i++) {
+            int prev = 0;
             for(int j = 1; j <= n; j++) {
-                if(text1[i - 1] == text2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                int temp = dp[j];
+                if(text1[i - 1] == text2[j - 1]) 
+                    dp[j] = prev + 1;
                 else 
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                    dp[j] = max(dp[j], dp[j - 1]);
+                prev = temp;
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };

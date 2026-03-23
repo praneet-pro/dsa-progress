da1430129bedf618b4611e66631a1182a55f7bd2
Praneet <spraneet333@gmail.com>
2026-03-23 16:41:41 +0530

    Time: 2 ms (81.37%), Space: 11.5 MB (84.21%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -4,20 +4,21 @@ public:
         if(s.size() < p.size()) return {};
         vector<int> res;
         int freq[26] = {0};
-        int k = p.size();
-        int diff = 0;
+        int k = p.size(), diff = 0;
         for(int i = 0; i < k; i++) {
             freq[p[i] - 'a']++;
             freq[s[i] - 'a']--;
         }
         for(int i = 0; i < 26; i++)
-            if(freq[i] != 0) diff++;
+            if(freq[i] != 0) diff++;                //freq denotes the need
         if(diff == 0) res.push_back(0);
         for(int i = k; i < s.size(); i++) {
+            // including new char
             if(freq[s[i] - 'a'] == 0) diff++;
             freq[s[i] - 'a']--;
-            if(freq[s[i] - 'a'] == 0) diff--;
+            if(freq[s[i] -  'a'] == 0) diff--;
 
+            //removing old char
             if(freq[s[i - k] - 'a'] == 0) diff++;
             freq[s[i - k] - 'a']++;
             if(freq[s[i - k] - 'a'] == 0) diff--;
@@ -27,3 +28,6 @@ public:
         return res;
     }
 };
+
+    
+    

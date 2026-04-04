6014a6b9b7a06604e2482a1d302ffcde33fa18fe
Praneet <spraneet333@gmail.com>
2026-04-04 11:08:08 +0530

    Time: 3 ms (77.9%), Space: 14 MB (86.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,14 +1,34 @@
+/*
+Topic: Greedy + Binary Search
+
+Invariant:
+- tails[i] stores the minimum possible ending value of an
+  increasing subsequence of length (i + 1).
+- tails is always sorted.
+
+Approach:
+- Traverse the array.
+- For each element x:
+  - If x is greater than all elements in tails, extend the LIS.
+  - Otherwise, replace the first element >= x using binary search
+    to keep tails minimal.
+- The size of tails gives the LIS length.
+
+Time Complexity: O(n log n)
+Space Complexity: O(n)
+*/
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[j] < nums[i])
-                    dp[i] = max(dp[i], dp[j] + 1);
-            }
+        vector<int> tails;
+        for(int x : nums) {
+            auto it  = lower_bound(tails.begin(), tails.end(), x);
+            if(it == tails.end()) 
+                tails.push_back(x);
+            else
+                *it = x;
         }
-        return *max_element(dp.begin(), dp.end());
+        return tails.size();
     }
 };

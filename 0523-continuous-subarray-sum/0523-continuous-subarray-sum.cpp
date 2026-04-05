37ea07beb782e16c074a60970f49d4b7a6ebe36d
Praneet <spraneet333@gmail.com>
2026-04-05 14:20:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -22,7 +22,7 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         if(k == 0) {
-            for(int i = 0; i < nums.size(); i++) 
+            for(int i = 1; i < nums.size(); i++) 
                 if(nums[i] == 0 && nums[i - 1] == 0) return true;
         return false;
         }

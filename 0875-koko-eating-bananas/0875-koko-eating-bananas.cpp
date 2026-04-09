8df2cd9eefa789f0787263db039f8936f5b69521
Praneet <spraneet333@gmail.com>
2026-04-09 17:26:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -7,7 +7,7 @@ public:
             int mid = low + (high - low)/2;
             int hrs = 0;
             for(int i = 0; i < piles.size(); i++) {
-                hrs += (piles[i] + mid - 1)/mid;    // instead of ceil
+                hrs += (piles[i] + mid - 1)/mid;    // instead of ceil (padding)
             }
             if(hrs <= h) high = mid;
             else low = mid + 1;

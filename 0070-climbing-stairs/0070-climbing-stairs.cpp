0539ed3730f366d0b5e1785afa25ea12f32d13a6
Praneet <spraneet333@gmail.com>
2026-04-17 18:59:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,15 +7,18 @@ public:
 
 private: 
     int sendScouts(int stairs_left, vector<int>& cache) {
+        // base conditions
         if(stairs_left == 0) return 1;
         if(stairs_left <= -1) return 0;
 
+        // check whether we've already calculated the path
         if(cache[stairs_left] != -1)
             return cache[stairs_left];
 
+        // recusion for 1 and 2 stairs
         int left_path = sendScouts(stairs_left - 1, cache);
         int right_path = sendScouts(stairs_left - 2, cache);
-
+        // calculation of the distinct ways for that path
         cache[stairs_left] = right_path + left_path;
 
         return cache[stairs_left];

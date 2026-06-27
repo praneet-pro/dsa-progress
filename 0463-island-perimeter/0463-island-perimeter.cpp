a3c25371a4814c64d57bbd84c3121a795b983715
Praneet <spraneet333@gmail.com>
2026-06-27 17:41:40 +0530

    Time: 0 ms (100%), Space: 102 MB (84.52%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int dr[4] = {-1, 1, 0, 0};
+        int dc[4] = {0, 0, -1, 1};
+        int peri = 0;
+
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < cols; c++) {
+                if(grid[r][c] == 1) {
+                    for(int d = 0; d < 4; d++) {
+                        int nr = r + dr[d];
+                        int nc = c + dc[d];
+
+                        if(nr < 0 || nc < 0 || nr >= rows || nc >= cols)
+                            peri++;
+                        else if(grid[nr][nc] == 0)
+                            peri++;
+                    }
+                }
+            }
+        }
+        return peri;
+    }
+};

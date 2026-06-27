4de040edda523b6a04742bf53b79fd5a5722582d
Praneet <spraneet333@gmail.com>
2026-06-27 15:48:46 +0530

    Time: 3 ms (95.27%), Space: 23 MB (46.85%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
+
+    bool is_pos(vector<int>& piles, int h, int k) {
+        int hrs = 0;
+        for(int i = 0; i < piles.size(); i++) {
+            hrs += (piles[i] + k - 1) / k;
+        }
+        return h >= hrs;
+    } 
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
+
         while(low < high) {
             int mid = low + (high - low)/2;
-            int hrs = 0;
-            for(int i = 0; i < piles.size(); i++) {
-                hrs += (piles[i] + mid - 1)/mid;    // instead of ceil (padding)
-            }
-            if(hrs <= h) high = mid;
-            else low = mid + 1;
+
+            if(is_pos(piles, h, mid))
+                high = mid;
+
+            else
+                low = mid + 1;
         }
         return low;
     }

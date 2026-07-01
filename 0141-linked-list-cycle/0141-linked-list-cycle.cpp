8323dca5c051e510a0185897f77816dfc06c2ee8
Praneet <spraneet333@gmail.com>
2026-07-01 18:03:38 +0530

    Time: 6 ms (92.52%), Space: 11.8 MB (53.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,17 +12,11 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next != NULL) {
+        while(fast && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast) 
-                return true;
+            if(slow == fast) return true;
         }
         return false;
     }
 };
-
-
-/*
-What happens if this linked list represents high-frequency trading data, and it has 10 billion nodes? If you write down 10 billion memory addresses on your notepad, you are going to consume gigabytes of extra RAM just to solve a simple loop. That is an O(N) memory solution. A systems engineer cannot afford that.
-*/
